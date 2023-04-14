078606c6b7faf794ef815ce56fea18dce4c16486
akbarriyaji <128137003+akbarriyaji@users.noreply.github.com>
2023-04-14 14:51:18 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Akbar</li>
+        <li>Nama Belakang : Riyaji</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Tomang Pulo II No.20 Jakarta Barat</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
